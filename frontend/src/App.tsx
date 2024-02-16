a28d1148824ffad6c858da9c6902e6d4934c9c87
AbhiAtiUttam <Abhi@glocalize.net>
2024-02-16 13:26:18 +0530

    react changed to vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,3 +30,22 @@ function App() {
 }
 
 export default App;
+
+// import { useState } from 'react'
+
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         hello
+// </div>
+//     </>
+//   )
+// }
+
+// export default App
+
