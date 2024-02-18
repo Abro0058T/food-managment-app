026aded093a5147181bc433565a112e010df68b4
AbhiAtiUttam <Abhi@glocalize.net>
2024-02-18 23:28:27 +0530

    docker file added

--- frontend/src/components/dashboard/CollectorDashBoard.jsx
+++ frontend/src/components/dashboard/CollectorDashBoard.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar'
 import CollectorMainBoard from "./CollectorMainBoard.jsx"
 import { useDispatch } from 'react-redux'
 import { Link, Outlet, useParams } from 'react-router-dom'
-import { MdHistory, MdOutlineSettings, MdOutlineSpaceDashboard } from 'react-icons/md'
+import { MdHistory, MdOutlineSettings, MdOutlineSpaceDashboard ,MdMap} from 'react-icons/md'
 import { fetchCollectorHistory } from '../../redux/restaurants/restaurantsActions.js'
 function CollectorDashBoard() {
     const {id}=useParams();
@@ -29,13 +29,21 @@ function CollectorDashBoard() {
                            Histoy
                             </a> 
                            </Link></div>
+                        <div className={style.sideButtons}><MdMap className={style.icons} />  <Link to="settings">
+                      
+                      <a className={style.linkColl}>
+                          Map 
+                          </a>  
+                          </Link></div>
                     <div className={style.sideButtons}><MdOutlineSettings className={style.icons} />  <Link to="settings">
                       
                     <a className={style.linkColl}>
                         Settings 
                         </a>  
                         </Link></div>
+
                 </div>
+                
                 <Outlet />
   </div>
   )
